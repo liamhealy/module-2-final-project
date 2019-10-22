2641d4e4e156c52b974bb1b10072535c43474aa2
Altaf Quadri <altafmquadri@gmail.com>
2019-10-22 16:35:44 -0400

    built CRU for users class

--- group-it/app/controllers/users_controller.rb
+++ group-it/app/controllers/users_controller.rb
@@ -1,2 +1,34 @@
 class UsersController < ApplicationController
+
+    def new
+        @user = User.new
+    end
+
+    def show
+        @user = User.find(params[:id])    
+    end
+
+    def create
+        @user = User.create(params_user)
+        redirect_to user_path(@user)
+    end
+
+    def edit
+        @user = User.find(params[:id])
+    end
+
+    def update
+        @user = User.find(params[:id])
+        @user.update(params_user)
+        redirect_to user_path(@user)    
+    end
+
+
+
+    private
+
+    def params_user
+        params.require(:user).permit(:username, :age, :location, :bio)
+    end
+
 end
