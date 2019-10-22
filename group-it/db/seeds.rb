31c1f50df3c201d6fe1b918f7cac811fa69214ea
jz0001 <jinming.zhang.92@gmail.com>
2019-10-22 13:46:07 -0400

    added seeds file using Faker, created associations

--- group-it/db/seeds.rb
+++ group-it/db/seeds.rb
@@ -5,3 +5,44 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.destroy_all
+Note.destroy_all
+Group.destroy_all
+Comment.destroy_all
+
+
+50.times do
+    username = Faker::Name.first_name
+    age = (20...50).to_a.sample
+    location = Faker::Nation.capital_city
+    bio = Faker::Job.title
+    User.create(username: username, age: age, location: location, bio: bio)
+  end
+
+10.times do
+    title = Faker::Company.name
+    theme = Faker::Types.rb_string
+    description = Faker::Marketing.buzzwords
+    Group.create(title: title, theme:theme, description: description)
+end
+
+50.times do
+    user_id = (1...50).to_a.sample
+    group_id = (1...10).to_a.sample
+    GroupMember.create(user_id:user_id, group_id:group_id)
+end
+
+100.times do
+    title = Faker::Marketing.buzzwords
+    content = Faker::Marketing.buzzwords
+    group_member_id = (1...50).to_a.sample
+    Note.create(title: title, content: content, group_member_id: group_member_id)
+end
+
+500.times do
+    note_id = (1...100).to_a.sample
+    content = Faker::Marketing.buzzwords
+    Comment.create(note_id:note_id, content:content)
+end
