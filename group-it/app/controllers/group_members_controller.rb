4baf16b364be2468cbd75af5e9dc9322b975d180
Liam <liam.healy1@marist.edu>
2019-10-23 14:38:01 -0400

    Added session functionality for signing users in/out. Need to continue working on it still.

--- group-it/app/controllers/group_members_controller.rb
+++ group-it/app/controllers/group_members_controller.rb
@@ -1,2 +1,21 @@
 class GroupMembersController < ApplicationController
+
+    def new
+        @group_member = GroupMember.new
+        @group = Group.find(session[:group_id])
+        # redirect_to create_group_member_path
+    end
+
+    def create
+        @group_member = GroupMember.new(user_id: session[:user_id], group_id: session[:group_id])
+        
+        if @group_member.save
+            flash[:notice] = "Successfully joined this group!"
+            redirect_to group_path(@group_member.group_id)
+        else
+            flash[:notice] = "Unable to join group."
+            redirect_to groups_path
+        end
+    end
+
 end
