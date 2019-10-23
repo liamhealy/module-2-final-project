78958cc8f03f2df90d4bdee8ec5b3647e95b767d
jz0001 <jinming.zhang.92@gmail.com>
2019-10-23 17:03:08 -0400

    pushing after git pull

--- group-it/config/routes.rb
+++ group-it/config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new', as: 'login'
   post 'sessions', to: 'sessions#create', as: 'sessions'
   delete 'sessions', to: 'sessions#destroy'
-  get '/groups/:group_id/notes/:note_id/comments/new', to: 'comments#new'
+  # get '/groups/:group_id/notes/:note_id/comments/new', to: 'comments#new'
   # get '/groups/:group_id/notes/:id/comments/new', to: 'comments#new'
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
